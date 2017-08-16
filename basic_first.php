49a7a03816ba7c813fd0e1d0325534f5ffc11e7a
AshishkrGoyal <15103140ashish@gmail.com>
2017-08-17 01:28:28 +0530

    basic_added

---
+++ basic_first.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ashish Goyal
+ * Date: 8/17/2017
+ * Time: 12:32 AM
+ */
+// for above it is known as docBlock commenting method
+
+
+//echo 'Ashish Kumar goyal it is your first phpScript';
+/*echo 'php learning phase is good'*/
+
+#echo 'php is a scripting lang'
+
+echo'Load to @AshishkrGoyal Git Account'
+?>
